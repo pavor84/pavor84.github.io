d2d5ac2ca90ba2e33757a8888b4f152448c6300b
Pavel Vorobkalov <pavor@yandex-team.ru>
2021-07-15 17:19:12 +0300

    add new click script & test pages

--- covid/click_script.js
+++ covid/click_script.js
@@ -1,1 +1,68 @@
-window.alert('click script')
+(function() {
+  if (!location.href.includes('from_alice') {
+    return;
+  }
+
+  let selector;
+  if (location.href.startsWith('https://immune.mos.ru/rf')) {
+    selector = '.nlk-button[type="submit"]';
+  } else if (location.href.startsWith('https://www.gosuslugi.ru/10602/1')) {
+    selector = '[href="https://www.gosuslugi.ru/10600/1"],[href="https://www.gosuslugi.ru/10601/1"]';
+  } else {
+    return;
+  }
+
+  const selectAndMaybeClickButton = (selector) => {
+    const button = document.querySelector(selector);
+    if (button) {
+      button.click();
+      return true;
+    }
+    return false;
+  };
+
+  const addObserver = () => {
+    const callback = function(mutationsList, observer) {
+      for (const mutation of mutationsList) {
+        if (mutation.type !== 'childList') {
+          return;
+        }
+        for (node of mutation.addedNodes) {
+          if (node.nodeType !== Node.ELEMENT_NODE) {
+            continue;
+          }
+          if (node.matches(selector)) {
+            node.click();
+            observer.disconnect();
+            return;
+          } else {
+            button = node.querySelector(selector);
+            if (button) {
+              button.click();
+              observer.disconnect();
+              return;
+            }
+          }
+        }
+      }
+    };
+
+    const observer = new MutationObserver(callback);
+    observer.observe(document.body, {
+        childList: true,
+        subtree: true,
+    });
+  }
+
+  if (document.readyState === 'complete') {
+    if (!selectAndMaybeClickButton(selector)) {
+      addObserver();
+    }
+  } else {
+    document.addEventListener('load', () => {
+      if (!selectAndMaybeClickButton(selector)) {
+        addObserver();
+      }
+    });
+  }
+})();
