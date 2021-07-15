00f7ca5fa30fced662ca53bd2e956862bea05465
Pavel Vorobkalov <pavor@yandex-team.ru>
2021-07-15 20:22:38 +0300

    update script for extracting svg

--- covid/qr_script.js
+++ covid/qr_script.js
@@ -1,1 +1,21 @@
-document.querySelector('img.vaccine-qr-img').src
+(function() {
+  if (location.href.includes('immune.mos.ru')) {
+    const svg = document.querySelector('svg#qrCode');
+    if (svg) {
+      let svgContent = svg.outerHTML;
+      const svgPrefix = '<svg';
+      if (!svgContent.startsWith(svgPrefix)) {
+        return;
+      }
+      const svgXmlns = 'xmlns="http://www.w3.org/2000/svg"';
+      if (!svgContent.includes(svgXmlns)) {
+        svgContent = svgPrefix + ' ' + svgXmlns + svgContent.substr(svgPrefix.length);
+      }
+      return 'data:image/svg+xml;base64,' + window.btoa(svgContent);
+    }
+  } else if (location.href.includes('gosuslugi.ru')) {
+    const img = document.querySelector('img.vaccine-qr-img');
+    if (!img) return;
+    return img.src;
+  }
+})();
