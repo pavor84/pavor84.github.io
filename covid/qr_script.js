0b788d9683f8ce58f3c3d7eed63c2710ae595e42
Pavel Vorobkalov <pavor@yandex-team.ru>
2021-07-16 17:58:32 +0300

    add support for parsing of another QR code

--- covid/qr_script.js
+++ covid/qr_script.js
@@ -2,20 +2,17 @@
   if (location.href.includes('immune.mos.ru')) {
     const svg = document.querySelector('svg#qrCode');
     if (svg) {
-      let svgContent = svg.outerHTML;
-      const svgPrefix = '<svg';
-      if (!svgContent.startsWith(svgPrefix)) {
-        return;
-      }
-      const svgXmlns = 'xmlns="http://www.w3.org/2000/svg"';
-      if (!svgContent.includes(svgXmlns)) {
-        svgContent = svgPrefix + ' ' + svgXmlns + svgContent.substr(svgPrefix.length);
-      }
-      return 'data:image/svg+xml;base64,' + window.btoa(svgContent);
+      svg.setAttribute('xmlns', 'http://www.w3.org/2000/svg');
+      return 'data:image/svg+xml;base64,' + window.btoa(svg.outerHTML);
     }
   } else if (location.href.includes('gosuslugi.ru')) {
-    const img = document.querySelector('img.vaccine-qr-img');
-    if (!img) return;
-    return img.src;
+    let img = document.querySelector('img.vaccine-qr-img');
+    if (!img) {
+      img = document.querySelector('img[alt="QR-code"]');
+    }
+
+    if (img) {
+      return img.src;
+    }
   }
 })();
